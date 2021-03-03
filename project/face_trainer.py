2fc85b1845641f8b21acd8b6bcb9b1c6d00b390a
Mahima Sunny <mahimas@uw.edu>
2021-03-03 08:42:34 +0000

    update

--- project/face_trainer.py
+++ project/face_trainer.py
@@ -1,57 +1,50 @@
-#Copyright Anirban Kar (anirbankar21@gmail.com)
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
+''''
+Training Multiple Faces stored on a DataBase:
+	==> Each face should have a unique numeric integer ID as 1, 2, 3, etc                       
+	==> LBPH computed model will be saved on trainer/ directory. (if it does not exist, pls create one)
+	==> for using PIL, install pillow library with "pip install pillow"
+Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition    
+Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18   
+'''
 
-import cv2,os
+import cv2
 import numpy as np
-from PIL import Image 
+from PIL import Image
+import os
 
-path = os.path.dirname(os.path.abspath(__file__))
-recognizer = cv2.createLBPHFaceRecognizer()
-faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml'.xml);
-dataPath = './dataset'
+# Path for face image database
+path = 'dataset'
 
-def get_images_and_labels(datapath):
-     image_paths = [os.path.join(datapath, f) for f in os.listdir(datapath)]
-     # images will contains face images
-     images = []
-     # labels will contains the label that is assigned to the image
-     labels = []
-     for image_path in image_paths:
-         # Read the image and convert to grayscale
-         image_pil = Image.open(image_path).convert('L')
-         # Convert the image format into numpy array
-         image = np.array(image_pil, 'uint8')
-         # Get the label of the image
-         nbr = int(os.path.split(image_path)[1].split(".")[0].replace("face-", ""))
-         #nbr=int(''.join(str(ord(c)) for c in nbr))
-         print(nbr)
-         # Detect the face in the image
-         faces = faceCascade.detectMultiScale(image)
-         # If face is detected, append the face to images and the label to labels
-         for (x, y, w, h) in faces:
-             images.append(image[y: y + h, x: x + w])
-             labels.append(nbr)
-             cv2.imshow("Adding faces to traning set...", image[y: y + h, x: x + w])
-             cv2.waitKey(10)
-     # return the images list and labels list
-     return images, labels
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+detector = cv2.CascadeClassifier("./haarcascade_frontalface_alt.xml");
 
+# function to get the images and label data
+def getImagesAndLabels(path):
 
-images, labels = get_images_and_labels(dataPath)
-cv2.imshow('test',images[0])
-cv2.waitKey(1)
+    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
+    faceSamples=[]
+    ids = []
 
-recognizer.train(images, np.array(labels))
-recognizer.save('.\trainer\trainer.yml')
-cv2.destroyAllWindows()
+    for imagePath in imagePaths:
+
+        PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
+        img_numpy = np.array(PIL_img,'uint8')
+
+        id = int(os.path.split(imagePath)[-1].split(".")[1])
+        faces = detector.detectMultiScale(img_numpy)
+
+        for (x,y,w,h) in faces:
+            faceSamples.append(img_numpy[y:y+h,x:x+w])
+            ids.append(id)
+
+    return faceSamples,ids
+
+print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
+faces,ids = getImagesAndLabels(path)
+recognizer.train(faces, np.array(ids))
+
+# Save the model into trainer/trainer.yml
+recognizer.write('trainer/trainer.yml') # recognizer.save() worked on Mac, but not on Pi
+
+# Print the numer of faces trained and end program
+print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))

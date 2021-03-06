a7dbf341f83c8b84250a2ddc1515e44ec28dffd4
Mahima Sunny <mahimasunnym@gmail.com>
2021-03-06 02:21:16 +0000

    recognition done

--- project/face_recognition_impl.py
+++ project/face_recognition_impl.py
@@ -1,119 +1,68 @@
-# -*- coding: utf-8 -*-
-from __future__ import print_function
-import click
-import os
-import re
-import face_recognition.api as face_recognition
-import multiprocessing
-import itertools
-import sys
-import PIL.Image
+import face_recognition
+from PIL import Image, ImageDraw
 import numpy as np
+import cv2
+from IPython.display import display
 
+TOLERANCE= 0.6
 
-def scan_known_people(known_people_folder):
-    known_names = []
-    known_face_encodings = []
+image = face_recognition.load_image_file("known_faces/mahima.jpg")
+face_locations = face_recognition.face_locations(image)
+# print(face_locations)
+# Find all facial features in all the faces in a image 
+face_landmarks_list = face_recognition.face_landmarks(image)
+# print(face_landmarks_list)
 
-    for file in image_files_in_folder(known_people_folder):
-        basename = os.path.splitext(os.path.basename(file))[0]
-        img = face_recognition.load_image_file(file)
-        encodings = face_recognition.face_encodings(img)
+mahima = face_recognition.load_image_file("known_faces/mahima.jpg")
+mahima_face_encoding = face_recognition.face_encodings(mahima)[0]
+kamala = face_recognition.load_image_file("known_faces/Kamala1.jpg")
+print(kamala.shape, mahima.shape)
+kamala_face_encoding= face_recognition.face_encodings(kamala)[0]
 
-        if len(encodings) > 1:
-            click.echo("WARNING: More than one face found in {}. Only considering the first face.".format(file))
+known_face_encodings = [mahima_face_encoding, kamala_face_encoding]
+known_face_names = ["Mahima" , "Kamala Harris"]
 
-        if len(encodings) == 0:
-            click.echo("WARNING: No faces found in {}. Ignoring file.".format(file))
-        else:
-            known_names.append(basename)
-            known_face_encodings.append(encodings[0])
+unknown_image = face_recognition.load_image_file("unknown_faces/unknown.jpg")
+face_locations = face_recognition.face_locations(unknown_image)
+face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
 
-    return known_names, known_face_encodings
+pil_image = Image.fromarray(unknown_image)
+# Create a Pillow ImageDraw Draw instance to draw
+draw = ImageDraw.Draw(pil_image)
+print(np.array(face_locations).shape)
 
+# matches = face_recognition.compare_faces(known_face_encodings, face_encodings)
+matches = face_recognition.compare_faces(known_face_encodings, face_encodings[0], TOLERANCE)
+# Instead, use face_distance to calculate similarities
+face_distances = face_recognition.face_distance(known_face_encodings, face_encodings[0])
+best_match_index = np.argmin(face_distances)
+name = "Unknown"
+if matches[best_match_index]:
+    name = known_face_names[best_match_index]
+# # # Draw a box around the face using the Pillow module
+(t,r,b,l)= face_locations[0]
+# image_out= cv2.imread('unknown_faces/unknown.jpg') 
+# # fontScale 
+# fontScale = 1
+# # Blue color in BGR 
+# color = (0, 0, 255) 
+# # Line thickness of 2 px 
+# thickness = 2
+# # org 
+# org = (50, 200) 
+# # font 
+# font = cv2.FONT_HERSHEY_SIMPLEX 
+# # Using cv2.putText() method 
+# image_out = cv2.putText(image_out, name, org, font,  
+#                 fontScale, color, thickness, cv2.LINE_AA)
+# (x,y,w,h) = face_locations[0]
+# cv2.rectangle(image_out,(x,y),(x+w,y+h),(255,255,0),2)
+# cv2.imwrite('./output/face_recognition.jpg',image_out)
+# cv2.waitKey(0)
 
-def print_result(filename, name, distance, show_distance=False):
-    if show_distance:
-        print("{},{},{}".format(filename, name, distance))
-    else:
-        print("{},{}".format(filename, name))
-
-
-def test_image(image_to_check, known_names, known_face_encodings, tolerance=0.6, show_distance=False):
-    unknown_image = face_recognition.load_image_file(image_to_check)
-
-    # Scale down image if it's giant so things run a little faster
-    if max(unknown_image.shape) > 1600:
-        pil_img = PIL.Image.fromarray(unknown_image)
-        pil_img.thumbnail((1600, 1600), PIL.Image.LANCZOS)
-        unknown_image = np.array(pil_img)
-
-    unknown_encodings = face_recognition.face_encodings(unknown_image)
-
-    for unknown_encoding in unknown_encodings:
-        distances = face_recognition.face_distance(known_face_encodings, unknown_encoding)
-        result = list(distances <= tolerance)
-
-        if True in result:
-            [print_result(image_to_check, name, distance, show_distance) for is_match, name, distance in zip(result, known_names, distances) if is_match]
-        else:
-            print_result(image_to_check, "unknown_person", None, show_distance)
-
-    if not unknown_encodings:
-        # print out fact that no faces were found in image
-        print_result(image_to_check, "no_persons_found", None, show_distance)
-
-
-def image_files_in_folder(folder):
-    return [os.path.join(folder, f) for f in os.listdir(folder) if re.match(r'.*\.(jpg|jpeg|png)', f, flags=re.I)]
-
-
-def process_images_in_process_pool(images_to_check, known_names, known_face_encodings, number_of_cpus, tolerance, show_distance):
-    if number_of_cpus == -1:
-        processes = None
-    else:
-        processes = number_of_cpus
-
-    # macOS will crash due to a bug in libdispatch if you don't use 'forkserver'
-    context = multiprocessing
-    if "forkserver" in multiprocessing.get_all_start_methods():
-        context = multiprocessing.get_context("forkserver")
-
-    pool = context.Pool(processes=processes)
-
-    function_parameters = zip(
-        images_to_check,
-        itertools.repeat(known_names),
-        itertools.repeat(known_face_encodings),
-        itertools.repeat(tolerance),
-        itertools.repeat(show_distance)
-    )
-
-    pool.starmap(test_image, function_parameters)
-
-
-@click.command()
-@click.argument('known_people_folder')
-@click.argument('image_to_check')
-@click.option('--cpus', default=1, help='number of CPU cores to use in parallel (can speed up processing lots of images). -1 means "use all in system"')
-@click.option('--tolerance', default=0.6, help='Tolerance for face comparisons. Default is 0.6. Lower this if you get multiple matches for the same person.')
-@click.option('--show-distance', default=False, type=bool, help='Output face distance. Useful for tweaking tolerance setting.')
-def main(known_people_folder, image_to_check, cpus, tolerance, show_distance):
-    known_names, known_face_encodings = scan_known_people(known_people_folder)
-
-    # Multi-core processing only supported on Python 3.4 or greater
-    if (sys.version_info < (3, 4)) and cpus != 1:
-        click.echo("WARNING: Multi-processing support requires Python 3.4 or greater. Falling back to single-threaded processing!")
-        cpus = 1
-
-    if os.path.isdir(image_to_check):
-        if cpus == 1:
-            [test_image(image_file, known_names, known_face_encodings, tolerance, show_distance) for image_file in image_files_in_folder(image_to_check)]
-        else:
-            process_images_in_process_pool(image_files_in_folder(image_to_check), known_names, known_face_encodings, cpus, tolerance, show_distance)
-    else:
-        test_image(image_to_check, known_names, known_face_encodings, tolerance, show_distance)
-
-
-if __name__ == "__main__":
-    main()
+draw.rectangle(((l, t), (r, b)), outline=(0, 0, 255))
+# Draw a label with a name below the face 
+width, height = draw.textsize(name)
+draw.rectangle(((l, b - height - 10), (r, b)), fill=(0, 0, 255),     outline=(0, 0, 255))
+draw.text((l + 6, b - height - 5), name, fill=(255, 255, 255, 255))
+pil_image.save("./output/face_recognition.jpg") 
